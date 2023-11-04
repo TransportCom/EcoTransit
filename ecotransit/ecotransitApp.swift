94f4a71eacb521a13ce28c66f138404d0b3d816a
mohamed achi <mohamed.achi@esprit.tn>
2023-11-04 11:38:51 +0100

    Initial Commit

---
+++ ecotransit/ecotransitApp.swift
@@ -0,0 +1,20 @@
+//
+//  ecotransitApp.swift
+//  ecotransit
+//
+//  Created by Mohamed Achi on 4/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct ecotransitApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
