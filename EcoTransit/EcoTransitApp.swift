e557e7297cef76d6c20fad53b611f28ed7703f24
imen <imen.benatig@esprit.tn>
2023-11-04 22:19:37 +0100

    Initial Commit

---
+++ EcoTransit/EcoTransitApp.swift
@@ -0,0 +1,20 @@
+//
+//  EcoTransitApp.swift
+//  EcoTransit
+//
+//  Created by imba on 4/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct EcoTransitApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
